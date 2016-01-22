94a3f46aeb9c97cf3d424e6305e8d814503e0180
empantikos <empantikos@snf-687781.(none)>
2016-01-22 23:35:13 +0200

    badi move

--- animation_Team5.py
+++ animation_Team5.py
@@ -8,16 +8,20 @@ ph = os.path.expanduser('~/public_html')
 # first set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 # we install the axes x from 0 to 2 and y from -2 to 2
-ax = plt.axes(xlim=(0,2), ylim=(-2, 2))
+ax = plt.axes(xlim=(0,2), ylim=(-3, 3))
 # set background colour
-ax.set_axis_bgcolor('grey')
+ax.set_axis_bgcolor('green')
 # drawing a line
-line, = ax.plot([], [], lw=2,color='white')
+line, = ax.plot([], [], lw=8,color='black')
 # drawing two circles with black perimeter and width of five mm
-circle, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='black',
-markersize=30, markeredgewidth = 5)
-circle1, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='black',
-markersize=30, markeredgewidth = 1)
+circle, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='yellow',
+markersize=15, markeredgewidth = 6)
+circle1, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='orange',
+markersize=10, markeredgewidth = 3)
+circle2, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='red',
+markersize=10, markeredgewidth = 2)
+circle3, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='blue',
+markersize=10, markeredgewidth = 2)
 
 # initialization of the line and the two circles: plot the background of each frame
 def init():
@@ -26,21 +30,23 @@ def init():
     circle1.set_data([],[])
     return line,circle,circle1
 
-# animation function of sin,tan and the circles. This is called sequentially
+# animation function of cos,tan and the circles. This is called sequentially
 def animate(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    x = np.linspace(0, 3, 800)
+    y = np.cos(3 * np.pi * (x - 0.015 * i))
     #Defining the tan function
-    y1 = np.tan(2*np.pi*(x-0.01*i))
+    y1 = np.tan(3*np.pi*(x-0.015*i))
     line.set_data(x, y)
     line.set_data(x,y1)
     # ploting the two circles on sin and tan functions
     circle.set_data(x,y)
     circle1.set_data(x,y1)
+    circle2.set_data(x,x)
+    circle3.set_data(x,-x)
     return line,circle,circle1
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
+                               frames=300, interval=40, blit=True)
 # saving the video, and in every second draws 30 frames
 anim.save(ph+"/animation_Team5.mp4", fps=30, extra_args=['-vcodec', 'libx264'])
