1a385e7365813de92168501c86694df79000582c
empantikos <empantikos@snf-687781.(none)>
2016-01-23 21:24:15 +0200

    dadi film

--- animation_Team5.py
+++ animation_Team5.py
@@ -8,21 +8,16 @@ ph = os.path.expanduser('~/public_html')
 # first set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 # we install the axes x from 0 to 2 and y from -2 to 2
-ax = plt.axes(xlim=(0,2), ylim=(-3, 3))
+ax = plt.axes(xlim=(0,2), ylim=(-2, 2))
 # set background colour
-ax.set_axis_bgcolor('green')
+ax.set_axis_bgcolor('black')
 # drawing a line
 line, = ax.plot([], [], lw=8,color='black')
 # drawing two circles with black perimeter and width of five mm
-circle, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='yellow',
-markersize=15, markeredgewidth = 6)
-circle1, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='orange',
-markersize=10, markeredgewidth = 3)
-circle2, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='red',
-markersize=10, markeredgewidth = 2)
-circle3, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='blue',
-markersize=10, markeredgewidth = 2)
-
+circle, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='black',
+markersize=50, markeredgewidth = 1)
+circle1, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='red',
+markersize=50, markeredgewidth = 1)
 # initialization of the line and the two circles: plot the background of each frame
 def init():
     line.set_data([], [])
@@ -32,17 +27,15 @@ def init():
 
 # animation function of cos,tan and the circles. This is called sequentially
 def animate(i):
-    x = np.linspace(0, 3, 800)
-    y = np.cos(3 * np.pi * (x - 0.015 * i))
+    x = np.linspace(0, 2, 1000)
+    y = np.cos(3 * np.pi * (x - 0.01 * i))
     #Defining the tan function
-    y1 = np.tan(3*np.pi*(x-0.015*i))
+    y1 = np.tan(3*np.pi*(x-0.01*i))
     line.set_data(x, y)
     line.set_data(x,y1)
     # ploting the two circles on sin and tan functions
     circle.set_data(x,y)
     circle1.set_data(x,y1)
-    circle2.set_data(x,x)
-    circle3.set_data(x,-x)
     return line,circle,circle1
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
