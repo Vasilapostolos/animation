ae99a11b9c6b64d40bf5be7d2cd126308516adfd
atektonidou <atektonidou@snf-687787.(none)>
2016-01-23 22:34:45 +0200

    filmara

--- animation_Team5.py
+++ animation_Team5.py
@@ -13,6 +13,7 @@ ax = plt.axes(xlim=(0,2), ylim=(-2, 2))
 ax.set_axis_bgcolor('black')
 # drawing a line
 line, = ax.plot([], [], lw=8,color='black')
+line1, = ax.plot([], [], lw=16,color='gold')
 # drawing two circles with black perimeter and width of five mm
 circle, =ax.plot([],[], 'o', markerfacecolor='w', markeredgecolor='black',
 markersize=50, markeredgewidth = 1)
@@ -21,22 +22,25 @@ markersize=50, markeredgewidth = 1)
 # initialization of the line and the two circles: plot the background of each frame
 def init():
     line.set_data([], [])
+    line1.set_data([], [])
     circle.set_data([],[])
     circle1.set_data([],[])
-    return line,circle,circle1
+    return line,line1,circle,circle1
 
 # animation function of cos,tan and the circles. This is called sequentially
 def animate(i):
     x = np.linspace(0, 2, 1000)
+    x1 = np.linspace(2, 0, 1000)
     y = np.cos(3 * np.pi * (x - 0.01 * i))
+    y1 = np.cos(1 *np.pi * (x1 - 0.01 * i))
     #Defining the tan function
     y1 = np.tan(3*np.pi*(x-0.01*i))
     line.set_data(x, y)
-    line.set_data(x,y1)
+    line1.set_data(x1,y1)
     # ploting the two circles on sin and tan functions
     circle.set_data(x,y)
     circle1.set_data(x,y1)
-    return line,circle,circle1
+    return line,line1,circle,circle1
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
